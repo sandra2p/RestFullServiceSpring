bcf76c9594c1f3fe098be757af5ebcf865f15b1b
sandra2p <71703693+sandra2p@users.noreply.github.com>
2022-02-07 10:37:58 +0100

    First commit with tehe complete exercise

---
+++ helloworld/src/main/java/com/chandana/helloworld/bean/Greeting.java
@@ -0,0 +1,28 @@
+package com.chandana.helloworld.bean;
+ 
+public class Greeting {
+ 
+    private String player;
+    private String message;
+ 
+    public Greeting(String player, String message) {
+        this.player = player;
+        this.message = message;
+    }
+ 
+    public String getPlayer() {
+        return player;
+    }
+ 
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+ 
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
